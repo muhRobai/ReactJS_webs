83c93b8d2375a13aca67a6748f8d6a938c5e930a
kesetbasah <robaimuh@gmail.com>
2019-08-04 14:35:03 +0700

    try git

--- src/Page/App.js
+++ src/Page/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Hello world oke
+          Hello world try 
         </p>
         <a
           className="App-link"
